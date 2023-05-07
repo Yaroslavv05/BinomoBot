e5ee62c52511be9a6ea77415ff065b03e0cb347d
Yarik <chornyyaroslav5@gmail.com>
2023-05-07 21:33:11 +0300

    update

--- telegram_bot/start.py
+++ telegram_bot/start.py
@@ -8,12 +8,9 @@ from aiogram import types, Dispatcher
 
 async def send_messages(dp):
     # Create tasks for the user functions, passing a message argument to each task
-    task0 = asyncio.create_task(user.welcome())
-    task1 = asyncio.create_task(user.bonus())
     task2 = asyncio.create_task(user.check_daily_time())
     task3 = asyncio.create_task(user.send_every_10_minutes())
-
-    await asyncio.gather(task0, task1, task2, task3)
+    await asyncio.gather(task2, task3)
 
 
 if __name__ == "__main__":
