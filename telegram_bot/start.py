72a931919d05a9c23c56d6558519262bef029b82
MRX580 <andrejchenko0511@mail.ru>
2023-05-08 11:25:55 +0300

    Update

--- telegram_bot/start.py
+++ telegram_bot/start.py
@@ -6,13 +6,18 @@ from telegram_bot.handlers import user
 from aiogram import types, Dispatcher
 
 
-async def send_messages(dp):
+async def send_messages():
     # Create tasks for the user functions, passing a message argument to each task
     task2 = asyncio.create_task(user.check_daily_time())
     task3 = asyncio.create_task(user.send_every_10_minutes())
-    await asyncio.gather(task2, task3)
+    await asyncio.gather(task2)
+
+
+async def main():
+    asyncio.create_task(send_messages())
+    user.register_handlers_client(dp)
+    await dp.start_polling()
 
 
 if __name__ == "__main__":
-    user.register_handlers_client(dp)
-    executor.start_polling(dp, skip_updates=True, on_startup=send_messages)
+    asyncio.run(main())
