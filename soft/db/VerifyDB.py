b4fa20e5b14f258208e444f8326dfbc7fc7ac8b1
MRX580 <andrejchenko0511@mail.ru>
2023-05-15 23:05:30 +0300

    Update

--- soft/db/VerifyDB.py
+++ soft/db/VerifyDB.py
@@ -9,11 +9,13 @@ class DataVerify:
         try:
             self.cur.execute(
                 "CREATE TABLE DataVerify(date, name_pair, plus_or_minus)")
+            self.cur.execute('CREATE TABLE IF NOT EXISTS delete_messages(message_id INT, time TEXT)')
             self.con.commit()
         except sqlite3.OperationalError:
             pass
 
-    def input_data(self, date, symbol, plus_or_minus):
+    def input_data(self, date, symbol, plus_or_minus, message_id):
+        self.cur.execute(f'INSERT INTO delete_messages VALUES (?, ?)', (message_id, date))
         self.cur.execute(f'INSERT INTO DataVerify VALUES (?, ?, ?)', (date, symbol, plus_or_minus))
         self.con.commit()
 
@@ -21,3 +23,8 @@ class DataVerify:
         now = datetime.now()
         formatted_date = now.strftime("%Y-%m-%d")
         return self.cur.execute(f'SELECT * FROM DataVerify WHERE date = "{formatted_date}"').fetchall()
+
+    def get_all_messages(self):
+        now = datetime.now()
+        formatted_date = now.strftime("%Y-%m-%d")
+        return self.cur.execute(f'SELECT * FROM delete_messages WHERE date = "{formatted_date}"').fetchall()
