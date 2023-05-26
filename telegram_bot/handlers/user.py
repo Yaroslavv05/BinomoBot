4a2386f4d6f9fc4933047de221440664a4db04c1
Yarik <chornyyaroslav5@gmail.com>
2023-05-26 13:35:51 +0200

    update

--- telegram_bot/handlers/user.py
+++ telegram_bot/handlers/user.py
@@ -28,7 +28,7 @@ async def send_every_10_minutes():
     loop = asyncio.get_running_loop()
     with concurrent.futures.ThreadPoolExecutor() as pool:
         while True:
-            # -1001878714474 прод
+            # -1001949621459 прод
             # -1001969551915 тест
             if datetime.today().weekday() <= 4:
                 if users.is_work_time()[1]:
@@ -39,21 +39,21 @@ async def send_every_10_minutes():
                         now = datetime.now()
                         today = now.strftime("%Y-%m-%d")
                         data = datainfotosignal.get_last_forcast()
-                        data_m = await bot.send_photo(-1001969551915, photo=open('screenshot.png', 'rb'),
+                        data_m = await bot.send_photo(-1001949621459, photo=open('screenshot.png', 'rb'),
                                              caption=f'Валютная пара: {data[0]}\n\nНаправление: НА {"ПРОДАЖУ 🔴" if data[1] == "SHORT" else "ПОКУПКУ 🟢"}\n\nЦена торгового актива: {data[4]} 💵\nВремя выхода: {data[3]} 🕖')
                         write.input_data2(today, data_m.message_id)
                         await asyncio.sleep(180)
                         now_price = float(get_now_price())
                         if now_price >= float(data[4]) and data[1] == 'LONG':
-                            data_m = await bot.send_message(-1001969551915, f'{data[0]}\n\n✅ Сигнал зашел')
+                            data_m = await bot.send_message(-1001949621459, f'{data[0]}\n\n✅ Сигнал зашел')
                             write.input_data(today, data[0], '+')
                             write.input_data2(today, data_m.message_id)
                         elif now_price <= float(data[4]) and data[1] == 'SHORT':
-                            data_m = await bot.send_message(-1001969551915, f'{data[0]}\n\n✅ Сигнал зашел')
+                            data_m = await bot.send_message(-1001949621459, f'{data[0]}\n\n✅ Сигнал зашел')
                             write.input_data(today, data[0], '+')
                             write.input_data2(today, data_m.message_id)
                         else:
-                            data_m = await bot.send_message(-1001969551915, f'{data[0]}\n\n❌ Сигнал не зашел')
+                            data_m = await bot.send_message(-1001949621459, f'{data[0]}\n\n❌ Сигнал не зашел')
                             write.input_data(today, data[0], '-')
                             write.input_data2(today, data_m.message_id)
                         await asyncio.sleep(120)
@@ -73,14 +73,14 @@ async def check_daily_time():
                     minus = int(all_signals * 0.3)
                     for i in data_verify.get_all_messages():
                         try:
-                            await bot.delete_message(-1001969551915, i[0])
+                            await bot.delete_message(-1001949621459, i[0])
                         except aiogram.utils.exceptions.MessageToDeleteNotFound:
                             pass
-                    await bot.send_photo(-1001969551915, photo=open('preview.jpg', 'rb'), caption=f'Всем добрый вечер 😊\n\nТорговый день закончен, сегодня было ({all_signals}) сделок из которых:\n✅ ({plus}) зашли\n❌ ({minus}) не зашло\n\nВсем хорошего вечера, пока ☺️')
+                    await bot.send_photo(-1001949621459, photo=open('preview.jpg', 'rb'), caption=f'Всем добрый вечер 😊\n\nТорговый день закончен, сегодня было ({all_signals}) сделок из которых:\n✅ ({plus}) зашли\n❌ ({minus}) не зашло\n\nВсем хорошего вечера, пока ☺️')
                     users.change_work_time(morning=False, evening=True)
             else:
                 if users.is_work_time()[2]:
-                    await bot.send_message(-1001969551915, 'Доброе утро трейдеры ❗️\nВас приветствует Boss_trade_bot  и '
+                    await bot.send_message(-1001949621459, 'Доброе утро трейдеры ❗️\nВас приветствует Boss_trade_bot  и '
                                                            'мы начинаем нашу торговлю 📈')
                     users.change_work_time(morning=True, evening=False)
             await asyncio.sleep(5)
